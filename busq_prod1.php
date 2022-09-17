0e3826a0a473b3399ab668b657e0da54e71b0b86
unknown <diablo1769856@hotmail.com>
2022-09-17 12:24:37 -0500

    proyecto

---
+++ busq_prod1.php
@@ -0,0 +1,89 @@
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>Buscar Producto</title>
+            <link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+</head>
+<body>
+<body id="top">
+<div class="bgded overlay padtop" style="background-image:url('images/demo/backgrounds/fondo1.png');"> 
+<header id="header" class="hoc clear">
+    <div id="logo" class="fl_left"> 
+    <h1><a href="index.php">SC-INNOVASOFT</a></h1> </div>
+    <nav id="mainav" class="fl_right"> 
+    <ul class="clear">
+        <li class="active"><a href="cuenta.php">Inicio</a></li>
+        <i class="fas fa-house-blank"></i>
+                  <li><a href="index.php">Log out<i class="fas fa-door-open"></i></a></li>
+                  
+                  </ul>
+                  </nav>
+  </header>
+  
+<div class="wrapper row3">
+  <main class="hoc container clear"> 
+    
+    <!-- main body -->
+    <!-- ################################################################################################ -->
+    <div class="wrapper row3">
+  <main class="hoc container clear">
+  <section id="services">
+      <div class="sectiontitle">
+        <p class="nospace font-x3">Drogueria JJ</p>
+        <h6 class="heading">Buscar Producto</h6>
+        </article>
+        </li>
+        <div id="contenido">
+    <div style="margin: auto; width: 800px; border-collapse: separate; border-spacing; 10px 5px;">
+ 
+    <br>
+    <form action="resultado.php" method="GET" style="border-collapse: separate; border-spacing: 10px 5px;">
+      
+    <br>
+    <label>Producto: <input type="text" id="nombre" name="nombre"><br></label>
+    
+    <br>
+    <label>Laboratorio: <input type="text" id="laboratorio" name="laboratorio" ><br></label>
+    
+    <br>
+    <button type="submit" class="btn btn-success">Buscar</button>
+  </form> 
+
+
+</div><br>
+ <!-- ################################################################################################ -->
+    <!-- / main body -->
+    <div class="clear"></div>
+  </main>
+</div>
+<div class="wrapper row4">
+  <footer id="footer" class="hoc clear"> 
+     <!-- ################################################################################################ -->
+  
+        </li>
+      </ul>
+    </div>
+    <!-- ################################################################################################ -->
+  </footer>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row5">
+  <div id="copyright" class="hoc clear"> 
+    <!-- ################################################################################################ -->
+    <p class="fl_left">Copyright &copy; 2022 - All Rights Reserved - <a href="#">Domain Name</a></p>
+    <p class="fl_right">Template by <a target="_blank" href="https://www.os-templates.com/" title="Free Website Templates">OS Templates</a></p>
+    <!-- ################################################################################################ -->
+  </div>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<a id="backtotop" href="#top"><i class="fas fa-chevron-up"></i></a>
+<!-- JAVASCRIPTS -->
+<script src="layout/scripts/jquery.min.js"></script>
+<script src="layout/scripts/jquery.backtotop.js"></script>
+<script src="layout/scripts/jquery.mobilemenu.js"></script>
+</body>
+</html>
